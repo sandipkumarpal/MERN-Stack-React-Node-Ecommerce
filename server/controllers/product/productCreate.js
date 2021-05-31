c2539c0491212b40c4d490b348cce57383e943eb
SandipDotball <sandip.pal18@gmail.com>
2021-05-31 21:33:58 +0530

    Add product validation

--- server/controllers/product/productCreate.js
+++ server/controllers/product/productCreate.js
@@ -13,11 +13,31 @@ const productCreate = async (req, res) => {
       if (err) {
         return res.status(400).json({ error: 'Image could not be uploaded!' })
       }
+      console.log({ fields, file })
+      const { name, description, price, category, quantity, shipping } = fields
+
+      if (
+        !name ||
+        !description ||
+        !price ||
+        !category ||
+        !quantity ||
+        !shipping
+      ) {
+        return res.status(400).json({ error: 'All fields are required!' })
+      }
+
       const create = new Product(fields)
+
       if (file.photo) {
+        // Bellow (1mb = 1000000) photo size
+        if (file.photo.size > 1000000) {
+          return res.status(400).json({ error: 'Image should be 1mb in size' })
+        }
         create.photo.data = file.photo.path
         create.photo.contentType = file.photo.type
       }
+
       create.save((err, result) => {
         if (err) {
           return res.status(400).json({ error: errorHandler(err) })
